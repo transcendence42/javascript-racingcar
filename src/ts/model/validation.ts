bac91f3d8b217b251cefb2a7178dc9f842695c3f
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-04 18:37:19 +0900

    fix: 이름에 스페이스를 포함하는 문제

--- src/ts/model/validation.ts
+++ src/ts/model/validation.ts
@@ -1,6 +1,6 @@
 export function checkNamesValidation(inputString: string): boolean {
   const rawNames: string[] = inputString.split(",");
-  const names: string[] = rawNames.filter(x => x.length <= 5);
+  const names: string[] = rawNames.map((x)=>x.trim()).filter((x) => x.length <= 5);
   if (rawNames.length != names.length || names.length > 5) {
     return false;
   }
