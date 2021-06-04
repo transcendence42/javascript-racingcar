a254301256f7050e97a84483a47e58f1c0bd0fc3
jwon42 <jwon@student.42seoul.kr>
2021-06-04 23:23:05 +0900

    feat: 이름, 스피너 renderer 추가

--- src/view/renderer.ts
+++ src/view/renderer.ts
@@ -1,4 +1,4 @@
-import { inputSection, progressSection, resultSection } from './templates/index.js';
+import { inputSection, progressSection, carNameDiv, spinnerDiv, resultSection } from './templates/index.js';
 
 const renderInputSection = (): void => {
   document.getElementById('app')!.innerHTML = inputSection();
@@ -8,14 +8,31 @@ const renderProgressSection = (): void => {
   document.getElementById('app')!.insertAdjacentHTML(`beforeend`, progressSection());
 };
 
-const renderCarNameDiv = (name: string): void => {};
+const renderCarNameDiv = (carNameArray: Array<string>): void => {
+  carNameArray.forEach((carName) => {
+    document.getElementsByClassName('mt-4 d-flex')[0].insertAdjacentHTML(`beforeend`, carNameDiv(carName));
+  });
+};
 
-const renderArrowDiv = (): void => {};
+const renderSpinnerDiv = (carNameArray: Array<string>): void => {
+  console.log(carNameArray.length)
+  for (let index = 0; index < carNameArray.length; index += 1) {
+    console.log(index);
+    document.getElementsByClassName('mr-2')[index].insertAdjacentHTML(`beforeend`, spinnerDiv());
+  }
+};
 
-const renderSpinnerDiv = (): void => {};
+const renderArrowDiv = (carNameArray: Array<string>): void => {};
 
 const renderResultSection = (winners: string): void => {
   document.getElementById('app')!.insertAdjacentHTML(`beforeend`, resultSection(winners));
 };
 
-export { renderInputSection, renderProgressSection, renderResultSection };
+export {
+  renderInputSection,
+  renderProgressSection,
+  renderCarNameDiv,
+  renderSpinnerDiv,
+  renderArrowDiv,
+  renderResultSection,
+};
