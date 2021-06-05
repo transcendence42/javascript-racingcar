8a19503f1ee738a72f9dff3d33307e54873f07af
jwon42 <jwon@student.42seoul.kr>
2021-06-05 15:11:47 +0900

    feat: 화살표 삽입 렌더러 추가

--- src/view/renderer.ts
+++ src/view/renderer.ts
@@ -1,4 +1,4 @@
-import { inputSection, progressSection, carNameDiv, spinnerDiv, resultSection } from './templates/index.js';
+import { inputSection, progressSection, carNameDiv, spinnerDiv, arrowDiv, resultSection } from './templates/index.js';
 
 const renderInputSection = (): void => {
   document.getElementById('app')!.innerHTML = inputSection();
@@ -16,7 +16,9 @@ const renderSpinnerDiv = (element: Element): void => {
   element.insertAdjacentHTML(`beforeend`, spinnerDiv());
 };
 
-const renderArrowDiv = (carNameArray: Array<string>): void => {};
+const renderArrowDiv = (element: Element): void => {
+  element.insertAdjacentHTML(`afterend`, arrowDiv());
+};
 
 const renderResultSection = (winners: string): void => {
   document.getElementById('app')!.insertAdjacentHTML(`beforeend`, resultSection(winners));
