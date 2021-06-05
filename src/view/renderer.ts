d1b1ffa0e551869cf2ff2618b386f54ea315a506
jwon42 <jwon@student.42seoul.kr>
2021-06-05 11:55:41 +0900

    refactor: 초기 렌더링 및 상황에 따른 단일 렌더링을 위해 view에서 반복 로직 제거

--- src/view/renderer.ts
+++ src/view/renderer.ts
@@ -8,18 +8,12 @@ const renderProgressSection = (): void => {
   document.getElementById('app')!.insertAdjacentHTML(`beforeend`, progressSection());
 };
 
-const renderCarNameDiv = (carNameArray: Array<string>): void => {
-  carNameArray.forEach((carName) => {
-    document.getElementsByClassName('mt-4 d-flex')[0].insertAdjacentHTML(`beforeend`, carNameDiv(carName));
-  });
+const renderCarNameDiv = (element: Element, carName: string): void => {
+  element.insertAdjacentHTML(`beforeend`, carNameDiv(carName));
 };
 
-const renderSpinnerDiv = (carNameArray: Array<string>): void => {
-  console.log(carNameArray.length)
-  for (let index = 0; index < carNameArray.length; index += 1) {
-    console.log(index);
-    document.getElementsByClassName('mr-2')[index].insertAdjacentHTML(`beforeend`, spinnerDiv());
-  }
+const renderSpinnerDiv = (element: Element): void => {
+  element.insertAdjacentHTML(`beforeend`, spinnerDiv());
 };
 
 const renderArrowDiv = (carNameArray: Array<string>): void => {};
