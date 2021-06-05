82327de338f23c85bfb3f8f2d23bf539cd4063cf
jwon42 <jwon@student.42seoul.kr>
2021-06-05 11:58:05 +0900

    refactor: event.ts 삭제

--- src/controller/event.ts
+++
@@ -1,6 +0,0 @@
-const setEventListener = (): void => {
-  document.getElementById('car-names-submit')!.addEventListener('click', () => getCarNames());
-  document.getElementById('racing-count-submit')!.addEventListener('click', () => getCount());
-};
-
-export { setEventListener };
